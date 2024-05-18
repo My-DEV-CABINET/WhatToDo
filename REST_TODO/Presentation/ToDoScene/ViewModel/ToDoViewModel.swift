f244c4f56923cc35e6038845621ede7498fb8a8b
jakkujakku <jakkujakku98@gmail.com>
2024-05-18 12:14:14 +0900

    [FEAT]: ViewModel 생성

---
+++ REST_TODO/Presentation/ToDoScene/ViewModel/ToDoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoViewModel.swift
+//  REST_TODO
+//
+//  Created by 준우의 MacBook 16 on 5/18/24.
+//
+
+import Foundation
+
+// To Do: ViewModel 작업 예정
+final class ToDoViewModel {
+    // To Do Task...
+}
